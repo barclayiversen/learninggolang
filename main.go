c9bf53a0c027074c2f5df0c363f6d50b2e29891e
barclayiversen <barclayiversen@gmail.com>
2019-04-18 20:33:45 -0700

    saved examples of useful code to files. Need to start building Http server

--- main.go
+++ main.go
@@ -1,51 +1,22 @@
 //package gogame
+// Haven't built an http server yet. Need to work out basics of requests in and out
 
 package main
 
 import (
 	"fmt"
+	"golang.org/x/crypto/bcrypt"
 	"time"
 	"math/rand"
+	"net/http"
 )
 
 
-var r = rand.New(rand.NewSource(time.Now().Unix())
-
-type person struct {
-	first string
-	last  string
-}
-
-type hero struct {
-	person
-	class string
-	strength   int
-	resilience int
-	agility    int
-}
-
-func (s hero) speak() {
-	fmt.Println("I am", s.first, s.last, " a strong ", s.class)
-}
-
-func (i hero) attack() {
-	luck := roll()
-	//fmt.Println(luck)
-	fmt.Println(i.first, i.strength)
-	fmt.Println(i.strength, luck, luck + i.strength)
-	x := luck + i.strength
-	fmt.Println(x)
-	//return "cool"
-}
 
 
-func roll() int {
-	//x := r.Intn(10)
-	//fmt.Println("you rolled a ", x)
-	//return x
-	fmt.Println("roll called")
-	return 3
-}
+
+
+
 
 func main() {
 	fmt.Println(currenttime)
