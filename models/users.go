21935645fd3cc159947b7d20e781389ef28a8b86
barclayiversen <barclayiversen@gmail.com>
2019-04-22 09:53:53 -0700

    Added some example datastore code to users model to start with.

--- models/users.go
+++ models/users.go
@@ -1,1 +1,34 @@
 package models
+
+import (
+	"cloud.google.com/go/datastore"
+	"fmt"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// Create a datastore client. In a typical application, you would create
+	// a single client which is reused for every datastore operation.
+	dsClient, err := datastore.NewClient(ctx, "barclay-test-env")
+	if err != nil {
+		// Handle error.
+	}
+
+	k := datastore.NameKey("Entity", "stringID", nil)
+	e := new(Entity)
+	if err := dsClient.Get(ctx, k, e); err != nil {
+		// Handle error.
+		fmt.Println(err)
+	}
+
+	old := e.Value
+	e.Value = "Hello World!"
+
+	if _, err := dsClient.Put(ctx, k, e); err != nil {
+		// Handle error.
+		fmt.Println(err)
+	}
+
+	fmt.Printf("Updated value from %q to %q\n", old, e.Value)
+}
