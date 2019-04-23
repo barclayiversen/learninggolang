ee3d040077748f1be5d40adc532f3f06d73cfe5f
barclayiversen <barclayiversen@gmail.com>
2019-04-23 09:47:26 -0700

    Cleaned up auth.go file
    Just started webdev golang course so things are about to change quickly.

--- app/auth.go
+++ app/auth.go
@@ -1,17 +1,6 @@
 package app
 
 import (
-	"context"
-	"fmt"
-	"github.com/dgrijalva/jwt-go"
-	"net/http"
-	"os"
-	"strings"
-)
-
-package app
-
-import (
 "net/http"
 u "lens/utils"
 "strings"
@@ -26,7 +15,7 @@ var JwtAuthentication = func(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		notAuth := []string{"/api/user/new", "/api/user/login"} //List of endpoints that doesn't require auth
+		notAuth := []string{"/api/user/new", "/api/user/login", "/api/healthcheck"} //List of endpoints that doesn't require auth
 		requestPath := r.URL.Path //current request path
 
 		//check if request does not need authentication, serve the request if it doesn't need it
