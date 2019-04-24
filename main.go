834d9389d33ebc42e32e50b5bd8039de552399c4
barclayiversen <barclayiversen@gmail.com>
2019-04-23 20:49:07 -0700

    trimming down main.go since starting web dev course.
    Goal is to simply get the http server running. Stripped to bare bones.

--- main.go
+++ main.go
@@ -15,22 +15,17 @@ import (
 func main() {
 
 	router := mux.NewRouter()
+	router.Use(app.JwtAuthentication) //attach JWT auth middleware
 
-	router.HandleFunc ( path: "/api/user/new", controllers.CreateAccount).Methods( methods: "POST")
-	router.HandleFunc ( path: "/api/user/login", controllers.Authenticate).Methods( methods: "POST")
-	router.HandleFunc ( path: "/api/games/new", controllers.CreateGame).Methods( methods: "POST")
-	//router.HandleFunc ( path: "/api/user/new", controllers.GetContactsFor).Methods( methods: "GET")
-
-	router.Use(app.JwtAuthentication)
-
-	port := os.Getenv( key: "PORT" )
+	port := os.Getenv("PORT") //Get port from .env file, we did not specify any port so this should return an empty string when tested locally
 	if port == "" {
-		port = "8000"
+		port = "8000" //localhost
 	}
 
-	err := http.ListenAndServe(":" + port, router)
-	if err != nil {
+	fmt.Println(port)
 
-		fmt.Println(err)
+	err := http.ListenAndServe(":" + port, router) //Launch the app, visit localhost:8000/api
+	if err != nil {
+		fmt.Print(err)
 	}
 }
