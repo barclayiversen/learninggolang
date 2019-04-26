2ff3d150efb1dd4614eee6aef8e9d68cadf1808d
barclayiversen <barclayiversen@gmail.com>
2019-04-25 19:20:40 -0700

    Updated main.go with first handler func

--- main.go
+++ main.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
+	//"github.com/gorilla/mux" // still deciding on a mux
+	//"github.com/julienschmidt/httprouter"
+	"io"
 	"net/http"
 	"os"
 )
 
+func index (res http.ResponseWriter, req *http.Request) {
+
+	res.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	io.WriteString(res,`
+	<!--not serving from our server-->
+	<img src="https://upload.wikimedia.org/wikipedia/commons/6/6e/Golde33443.jpg">
+	`)
+}
+
 func main() {
 
-	router := mux.NewRouter()
+
+	router := http.NewServeMux()
+	//router := mux.NewRouter()
 	//router.Use(app.JwtAuthentication) //attach JWT auth middleware
 
 	port := os.Getenv("PORT") //Get port from .env file, we did not specify any port so this should return an empty string when tested locally
@@ -18,7 +32,7 @@ func main() {
 	}
 
 	fmt.Println(port)
-
+	router.Handle("/", http.HandlerFunc(index))
 	err := http.ListenAndServe(":" + port, router) //Launch the app, visit localhost:8000/api
 	if err != nil {
 		fmt.Print(err)
